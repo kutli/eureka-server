046ac247901f560c5bf2fdeda7da8150ed64c5b1
kutli <kutlidev@gmail.com>
2022-09-26 18:26:26 -0500

    ignoringAntMatchers for WebSecurityConfigurerAdapter

--- src/main/java/com/kutli/eurekaserver/config/SecurityConfig.java
+++ src/main/java/com/kutli/eurekaserver/config/SecurityConfig.java
@@ -9,6 +9,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
     }
 }
